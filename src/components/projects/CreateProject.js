b184d7e637876ae8c17ce65a06c0e22d2340c027
Ayomirotoye <ayomirotoye@gmail.com>
2020-03-09 10:44:49 +0100

    add: syncing ui with firestore collection

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { createProject } from "../../store/actions/projectActions";
 
 export class CreateProject extends Component {
     state = {
@@ -13,7 +15,8 @@ export class CreateProject extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state);
     }
     render() {
         return (
@@ -30,7 +33,7 @@ export class CreateProject extends Component {
                     </div>
                    
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Create</button>
+                        <button className="btn pink lighten-1 z-depth-0" onClick={this.handleSubmit}>Create</button>
                     </div>
                 </form>
             </div>
@@ -38,4 +41,10 @@ export class CreateProject extends Component {
     }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        createProject: (project) => dispatch(createProject(project)) 
+    }
+}
+export default connect(null, mapDispatchToProps)(CreateProject);
+
